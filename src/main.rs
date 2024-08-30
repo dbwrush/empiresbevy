44f87cd295fec26442d4c2786b8ac834d78efaa2
David Rush <43554173+dbwrush@users.noreply.github.com>
2024-08-30 17:56:09 -0500

    Begin implementing cell behavior

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,27 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::HashMap};
 use noise::{NoiseFn, Simplex};
 use rand::Rng;
 use rayon::prelude::*;
 use std::time::Instant;
 
-const WIDTH: usize = 720;
-const HEIGHT: usize = 480;
-const VARIABLES: usize = 3; // Example: 0 = terrain, 1 = empire, 2 = strength
+const WIDTH: usize = 426;
+const HEIGHT: usize = 240;
+const VARIABLES: usize = 4; // Terrain, strength, empire
 const OCEAN_CUTOFF: f32 = 0.4;
 
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
     app.add_systems(Startup, setup);
-    app.add_systems(Update, (update_cells,update_colors, draw_fps));
+    app.add_systems(Update, (update_colors, draw_fps));
+    app.add_systems(Update, pull_system);
+    app.add_systems(PostUpdate, push_system);
     app.insert_resource(RenderMode::StrengthView);
+    app.insert_resource(CellMap(HashMap::default()));
     app.run();
 }
 
-fn setup(mut commands: Commands, windows: Query<&mut Window>) {
+fn setup(mut commands: Commands, windows: Query<&mut Window>, mut cell_map: ResMut<CellMap>) {
     let window_width = windows.iter().next().unwrap().width();
     let window_height = windows.iter().next().unwrap().height();
     let scale_x = WIDTH as f32 / window_width;
@@ -33,7 +36,8 @@ fn setup(mut commands: Commands, windows: Query<&mut Window>) {
         },
         ..Default::default()
     });
-    commands.insert_resource(Grid::new(WIDTH, HEIGHT, VARIABLES));
+
+    let grid = Grid::new(WIDTH, HEIGHT, VARIABLES);
 
     commands.spawn(TextBundle {
         text: Text::from_section(
@@ -53,6 +57,7 @@ fn setup(mut commands: Commands, windows: Query<&mut Window>) {
     // Initialize sprites
     for x in 0..WIDTH {
         for y in 0..HEIGHT {
+            let terrain = grid.data[x][y][0];
             commands.spawn(SpriteBundle {
                 sprite: Sprite {
                     color: Color::WHITE,
@@ -62,9 +67,19 @@ fn setup(mut commands: Commands, windows: Query<&mut Window>) {
                 transform: Transform::from_xyz(x as f32, y as f32, 0.0),
                 ..Default::default()
             });
+            if terrain > OCEAN_CUTOFF {
+                let entity = commands.spawn(Cell::new(x, y)).id();
+                cell_map.0.insert((x, y), entity);
+            }
+
         }
     }
+
+    commands.insert_resource(grid);
 }
+
+#[derive(Resource)]
+struct CellMap(HashMap<(usize, usize), Entity>);
 
 #[derive(Resource)]
 struct Grid {
@@ -87,51 +102,68 @@ impl Grid {
             });
         });
 
-        for _ in 0..100 {
-            let x = rng.gen_range(0..WIDTH);
-            let y = rng.gen_range(0..HEIGHT);
-            if data[x][y][0] >= OCEAN_CUTOFF {
-                data[x][y][1] = rng.gen_range(0.0..100.0);
-            }
+        Grid { data}
+    }
+}
+
+#[derive(Component)]
+struct Cell {
+    empire: i32,
+    strength: f32,
+    need: f32,
+    position: (usize, usize),
+    send_amount: f32,
+    send_target: (usize, usize),
+    send_empire: i32, //this is the empire which commits an attack, in case a cell changes hands while it has already committed forces to an attack
+    //send_empire should be updated to the owner of the cell during the push phase.
+    //consider adding a pointer to all land neighbors (ignore ocean since no entities are made for ocean)
+}
+
+impl Cell {
+    fn new(x: usize, y: usize) -> Self {
+        Cell {
+            empire: -1,
+            strength: 0.0,
+            need: 0.0,
+            position: (x, y),
+            send_amount: 0.0,
+            send_target: (0, 0),
+            send_empire: -1,
         }
-
-        Grid { data }
     }
 
-    fn update(&mut self) {
-        self.data.par_iter_mut().for_each(|row| {
-            row.iter_mut().for_each(|cell| {//strength phase
-                if cell[1] != -1.0 {
-                    cell[2] += 1.0 - (OCEAN_CUTOFF - cell[0]).abs();
-                    cell[2] *= 0.99;
-                }
-            });
-        });
-        self.data.par_iter_mut().for_each(|row| {
-            row.iter_mut().for_each(|cell| {//attack phase
-                // Update cell logic here
-                // Example update
-            });
-        });
-        self.data.par_iter_mut().for_each(|row| {
-            row.iter_mut().for_each(|cell| {//need phase
-                // Update cell logic here
-                // Example update
-            });
-        });
-        self.data.par_iter_mut().for_each(|row| {
-            row.iter_mut().for_each(|cell| {//need spread phase
-                // Update cell logic here
-                // Example update
-            });
-        });
-        self.data.par_iter_mut().for_each(|row| {
-            row.iter_mut().for_each(|cell| {//resource phase
-                // Update cell logic here
-                // Example update
-            });
-        });
+    fn push(&mut self, neighbors: &HashMap<(usize, usize), Entity>) {//I call this 'push' because the cell is reading data from neighbors and pushing a decision
+        // Read need, strength, and owner data from neighbors (no need for it to be mutable), use it to decide if a cell should hold, send reinforcements,
+        // or attack a neighbor
+        // Cells need 3x strength to win an attack, so they can afford to have 1/3 the strength of the strongest enemy neighbor
+        // If strength is > 0.3x the strongest enemy neighbor, it can survive attacks, extra strength can be sent as attacks or reinforcements
+        // Check neighboring friendly (same empire) cells for need, send extra strength to them first in order of need
+        // Check neighboring enemy cells for strength, if extra strength is > 0.3x the weakest enemy then attack the weakest enemy
     }
+
+    fn pull(&mut self, neighbors: &HashMap<(usize, usize), Entity>) {//I call this 'pull' because the cell is pulling the decisions from other cells to update its own data
+        // Check the send_ variables of all neighbors to see if they are sending strength to this cell
+        // First add reinforcements from friendly cells to this cell's strength
+        // Then divide incoming strength from enemy cells by 3 and subtract it from this cell's strength. Handle attacks from weakest to strongest.
+        // If an attack causes strength to go below 0, change this cell's owner to the attacking empire and multiply strength by -1, all further attacks will be considered reinforcements
+
+        // Update cell data in the grid for rendering.
+
+        // Use terrain data from the grid to determine how much strength this cell should generate. The closer to ocean level, the more strength is made.
+        // Multiply strength by 0.99 so it can't just go up forever.
+    }
+}
+
+fn push_system(mut query: Query<&mut Cell>, cell_map: Res<CellMap>) {
+    query.par_iter_mut().for_each(|mut cell| {
+        cell.push(&cell_map.0);
+    });
+}
+
+fn pull_system(mut query: Query<&mut Cell>, cell_map: Res<CellMap>) {
+    query.par_iter_mut().for_each(|mut cell| {
+        cell.pull(&cell_map.0);
+    });
 }
 
 #[derive(Resource)]
@@ -140,10 +172,6 @@ enum RenderMode {
     EmpireView,
     TerrainView,
     // Add more render modes here
-}
-
-fn update_cells(mut grid: ResMut<Grid>) {
-    grid.update();
 }
 
 fn update_colors(
